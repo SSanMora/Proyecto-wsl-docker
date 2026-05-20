750e403868694da9c73d792ff830c81f96426fc5
Santiago Serrano Morales <serrano.santiago@correounivalle.edu.co>
2026-05-20 08:53:33 -0500

    feat: estructura de servicios docker por Santiago Serrano Morales

---
+++ node-app/index.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Entorno Controlled WSL2</title>
+</head>
+<body>
+    <h1>¡Servidor Nginx funcionando en WSL2!</h1>
+    <p>Grupo de Estudiantes - Infraestructura de Contenedores.</p>
+</body>
+</html>

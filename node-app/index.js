209bae8276714731bb0dce099a188cffae50353b
Samuel Esteban Peña Jaramillo <samuel.pena@correounivalle.edu.co>
2026-05-20 22:00:58 -0500

    Fix: Corrección de configuración Nginx, Dockerfile de Node y dependencias de Express

--- node-app/index.js
+++ node-app/index.js
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Entorno Controlled WSL2</title>
-</head>
-<body>
-    <h1>¡Servidor Nginx funcionando en WSL2!</h1>
-    <p>Grupo de Estudiantes - Infraestructura de Contenedores.</p>
-</body>
-</html>
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/api', (req, res) => {
+    res.json({
+        status: "success",
+        message: "API Node.js segura y respondiendo a través del proxy inverso de Nginx en WSL2",
+        timestamp: new Date()
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor de la API corriendo en el puerto ${PORT}`);
+});
